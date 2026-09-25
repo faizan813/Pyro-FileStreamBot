5c96976dfec117277709a6eae25c5d77f48bd622
faizan813 <f3983687@gmail.com>
2026-09-25 17:01:01 +0530

    Update __init__.py

--- WebStreamer/__init__.py
+++ WebStreamer/__init__.py
@@ -1,2 +1,16 @@
-# This file is a part of TG-FileStreamBot
-# Coding : Jyothis Jayanth [@EverythingSuckz]
+import logging
+from pyrogram import Client
+from WebStreamer.vars import Var
+import pyrogram.session.session
+
+# Force the library to skip heavy client clock validation and sync with Telegram
+pyrogram.session.session.Session.START_TIMEOUT = 60
+
+StreamBot = Client(
+    session_name="WebStreamer",
+    api_id=Var.API_ID,
+    api_hash=Var.API_HASH,
+    bot_token=Var.BOT_TOKEN,
+    sleep_threshold=Var.SLEEP_THRESHOLD,
+    workers=Var.WORKERS
+)
